36934389493a453a917f6113940d853953f95205
sakthivel280 <sakthivel.hsr06@gmail.com>
2026-07-13 09:26:08 +0530

    Complete setup and database migration

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,19 @@
 #storage place for objects that multiple files need to use.
 from flask import Flask
 from config import Config # for importing the config file's contents
-from extensions import db # for importing the db object from extensions.py
+from extensions import db,migrate # for importing the db and migrate objects from extensions.py
+from models.user import User # for importing the User model from models/user.py
 
-app = Flask(__name__)
-app.config.from_object(Config)
+app = Flask(__name__) # create a Flask application instance
+app.config.from_object(Config) # load the configuration settings from the Config class
 
-db.init_app(app)
+db.init_app(app) # initialize the database with the Flask application instance
+migrate.init_app(app, db) # initialize the migration functionality with the Flask application instance
 
-@app.route("/")
+@app.route("/") 
 def home():
     return "Internal Complaint Portal Backend is Running!"
+
 @app.route("/health")
 def health():
     return "Health Check Passed"
--- backend/extensions.py
+++ backend/extensions.py
@@ -1,4 +1,6 @@
-#Create shared objects that the whole project can use.
-from flask_sqlalchemy import SQLAlchemy
+# This file is for initializing the database and migration objects that will be used across the application.
+from flask_sqlalchemy import SQLAlchemy # for importing the SQLAlchemy class from flask_sqlalchemy
+from flask_migrate import Migrate # for importing the Migrate class from flask_migrate
 
-db = SQLAlchemy()
+db = SQLAlchemy() # create an instance of the SQLAlchemy class, which will be used to interact with the database
+migrate = Migrate() # create an instance of the Migrate class, which will be used to handle database migrations
